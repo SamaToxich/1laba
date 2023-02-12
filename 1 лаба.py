d7ff053b0795ea7b177a2fa78110139ef0fd8862
SamaToxich <125073984+SamaToxich@users.noreply.github.com>
2023-02-12 18:18:28 +0400

    Add files via upload

--- 1 лаба.py
+++ 1 лаба.py
@@ -1,11 +1,11 @@
 """Вариант 9.
-Нечетные четырехричные числа, не превышающие 102410, у которых вторая справа цифра равна 3. Выводит на
+Нечетные четырехричные числа, не превышающие 1024, у которых вторая справа цифра равна 3. Выводит на
 экран цифры числа, исключая тройки. Вычисляется среднее число между минимальным и максимальным и выводится прописью. """
 
-
 m = []
-chisla = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-buffer_len = 1  # размер буфера чтения
+chisla = ['0', '1', '2', '3']
+simvols = [' ', '!', '?', '.', ',', '\n', '']
+buffer_len = 1 
 work_buffer = ''
 
 
@@ -18,18 +18,9 @@ def chet(v):
 
 
 def slovo(x):
-    z = {'0': 'ноль',
-         '1': 'один',
-         '2': 'два',
-         '3': 'три',
-         '4': 'четыре',
-         '5': 'пять',
-         '6': 'шесть',
-         '7': 'семь',
-         '8': 'восемь',
-         '9': 'девять'
-         }
+    z = {'0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре', '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'}
     return z[x]
+
 
 with open('test.txt') as f:
     buffer = f.read(buffer_len)
@@ -37,9 +28,8 @@ with open('test.txt') as f:
         print('Файл пустой.')
         quit()
     while buffer:
-        while '0' <= buffer <= '3':
-            if '0' <= buffer <= '3':
-                work_buffer += buffer
+        while buffer not in simvols:
+            work_buffer += buffer
             buffer = f.read(buffer_len)
         if len(work_buffer) > 0:
             flag = True
@@ -47,10 +37,10 @@ with open('test.txt') as f:
                 if work_buffer[i] not in chisla:
                     flag = False
             if flag and len(work_buffer) > 1:
-                if work_buffer[-2] == '3' and int(work_buffer) < chet(1025) and int(work_buffer) % 2 == 1:
+                if work_buffer[0] != '0' and work_buffer[-2] == '3' and int(work_buffer) < chet(1025) and int(work_buffer) % 2 == 1:
                     m += [int(work_buffer)]
                     l = work_buffer.replace('3', '')
-                    print(l)
+                    print(f'Вывод: {l}')
         work_buffer = ''
         buffer = f.read(buffer_len)
 
